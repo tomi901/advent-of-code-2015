f89ae42537213e390c94bf2b488dd059a0af36cb
Tomás Rinaldi <29787965+tomi901@users.noreply.github.com>
2026-09-24 16:24:50 -0300

    Day 2 - Part 2

--- day_02/src/lib.rs
+++ day_02/src/lib.rs
@@ -12,15 +12,15 @@ impl FromStr for GiftBox {
 
     fn from_str(s: &str) -> Result<Self, Self::Err> {
         let mut split = s.split('x');
-        
+
         let length = split.next().context("length missing")?.parse()?;
         let width = split.next().context("width missing")?.parse()?;
         let height = split.next().context("height missing")?.parse()?;
-        
+
         if split.next().is_some() {
             return Err(anyhow!("too many dimensions, 3 expected"));
         }
-        
+
         Ok(Self {
             length,
             width,
@@ -30,13 +30,31 @@ impl FromStr for GiftBox {
 }
 
 impl GiftBox {
+    fn volume(&self) -> u64 {
+        self.length * self.width * self.height
+    }
+    
+    fn perimeters(&self) -> [u64; 3] {
+        [
+            self.length * 2 + self.width * 2,
+            self.width * 2 + self.height * 2,
+            self.length * 2 + self.height * 2,
+        ]
+    }
+
     fn surface_faces(&self) -> [u64; 3] {
         [self.length * self.width, self.width * self.height, self.length * self.height]
     }
-    
-    pub fn surface_area_needed(&self) -> u64 {
+
+    pub fn needed_wrap_surface_area(&self) -> u64 {
         let faces = self.surface_faces();
         let smallest = faces.iter().min().unwrap();
         faces.iter().sum::<u64>() * 2 + smallest
+    }
+    
+    pub fn needed_ribbon_length(&self) -> u64 {
+        let perimeters = self.perimeters();
+        let smallest = perimeters.iter().min().unwrap();
+        smallest + self.volume()
     }
 }
--- day_02/src/main.rs
+++ day_02/src/main.rs
@@ -1,4 +1,5 @@
 use std::path::Path;
+use std::str::FromStr;
 use anyhow::{self, Context};
 use day_02::GiftBox;
 use xmas::display_result;
@@ -17,11 +18,11 @@ fn main() -> anyhow::Result<()> {
 
 fn part_1(input: &str) -> anyhow::Result<()> {
     println!("Part 1:");
-    
+
     let mut result = 0;
     for line in input.lines() {
         let gift = line.parse::<GiftBox>()?;
-        result += gift.surface_area_needed();
+        result += gift.needed_wrap_surface_area();
     }
 
     display_result(&result);
@@ -31,5 +32,31 @@ fn part_1(input: &str) -> anyhow::Result<()> {
 fn part_2(input: &str) -> anyhow::Result<()> {
     println!("Part 2:");
 
+    let mut result = 0;
+    for line in input.lines() {
+        let gift =  GiftBox::from_str(line)?;
+        result += gift.needed_ribbon_length();
+    }
+
+    display_result(&result);
     Ok(())
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    pub fn part_2_case_1() {
+        let gift = GiftBox::from_str("2x3x4").unwrap();
+        let length = gift.needed_ribbon_length();
+        assert_eq!(length, 34);
+    }
+
+    #[test]
+    pub fn part_2_case_2() {
+        let gift = GiftBox::from_str("1x1x10").unwrap();
+        let length = gift.needed_ribbon_length();
+        assert_eq!(length, 14);
+    }
 }
