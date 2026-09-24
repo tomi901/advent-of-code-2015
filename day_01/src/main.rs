217fcf837d729d24a038c1cad4a161735ca95cd9
Tomás Rinaldi <29787965+tomi901@users.noreply.github.com>
2026-09-24 15:16:50 -0300

    Day 1 Part 1 and 2

--- day_01/src/main.rs
+++ day_01/src/main.rs
@@ -1,4 +1,4 @@
-use anyhow::{self, Context};
+use anyhow::{anyhow, Context};
 use xmas::display_result;
 
 fn main() -> anyhow::Result<()> {
@@ -10,14 +10,41 @@ fn main() -> anyhow::Result<()> {
 
 fn part_1() -> anyhow::Result<()> {
     println!("Part 1:");
-    let input = std::fs::read_to_string("./input.txt").context("Error reading input file.")?;
+    let input = std::fs::read_to_string("./input.txt")
+        .context("Error reading input file.")?;
 
-    // display_result(&result);
+    let result: i64 = input
+        .chars()
+        .map(|c| match c {
+            '(' => 1,
+            ')' => -1,
+            _ => panic!("Invalid character '{}'", c),
+        })
+        .sum();
+    
+    display_result(&result);
     Ok(())
 }
 
 fn part_2() -> anyhow::Result<()> {
     println!("Part 2:");
+    let input = std::fs::read_to_string("./input.txt").context("Error reading input file.")?;
 
-    Ok(())
+    let mut position = 0;
+    let mut floor = 0;
+    for char in input.chars() {
+        position += 1;
+        match char {
+            '(' => floor += 1,
+            ')' => floor -= 1,
+            _ => panic!("Invalid character '{}'", char),
+        }
+
+        if floor < 0 {
+            display_result(&position);
+            return Ok(());
+        }
+    }
+
+    Err(anyhow!("Invalid input, didn't reach -1"))
 }
