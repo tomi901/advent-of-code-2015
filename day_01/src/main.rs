882bf5cabc8489d0cd5295c3fb40235e40ca1edf
Tomás Rinaldi <29787965+tomi901@users.noreply.github.com>
2026-09-24 15:49:51 -0300

    Improved template

--- day_01/src/main.rs
+++ day_01/src/main.rs
@@ -1,50 +1,52 @@
+use std::path::Path;
 use anyhow::{anyhow, Context};
 use xmas::display_result;
 
 fn main() -> anyhow::Result<()> {
-    part_1()?;
+    let path = Path::new(env!("CARGO_MANIFEST_DIR"))
+        .join("input.txt");
+    let input = std::fs::read_to_string(path)
+        .context("Error reading input file.")?;
+    
+    part_1(&input)?;
     println!();
-    part_2()?;
+    part_2(&input)?;
     Ok(())
 }
 
-fn part_1() -> anyhow::Result<()> {
+fn part_1(input: &str) -> anyhow::Result<()> {
     println!("Part 1:");
-    let input = std::fs::read_to_string("./input.txt")
-        .context("Error reading input file.")?;
 
     let result: i64 = input
         .chars()
-        .map(|c| match c {
-            '(' => 1,
-            ')' => -1,
-            _ => panic!("Invalid character '{}'", c),
-        })
-        .sum();
+        .try_fold(0i64, |acc, c| {
+            get_movement(c).map(|mov| acc + mov)
+        })?;
     
     display_result(&result);
     Ok(())
 }
 
-fn part_2() -> anyhow::Result<()> {
+fn part_2(input: &str) -> anyhow::Result<()> {
     println!("Part 2:");
-    let input = std::fs::read_to_string("./input.txt").context("Error reading input file.")?;
 
-    let mut position = 0;
     let mut floor = 0;
-    for char in input.chars() {
-        position += 1;
-        match char {
-            '(' => floor += 1,
-            ')' => floor -= 1,
-            _ => panic!("Invalid character '{}'", char),
-        }
+    for (index, char) in input.chars().enumerate() {
+        floor += get_movement(char)?;
 
         if floor < 0 {
-            display_result(&position);
+            display_result(&(index + 1));
             return Ok(());
         }
     }
 
     Err(anyhow!("Invalid input, didn't reach -1"))
+}
+
+fn get_movement(c: char) -> anyhow::Result<i64> {
+    match c {
+        '(' => Ok(1),
+        ')' => Ok(-1),
+        _ => Err(anyhow!("Invalid character '{}'", c)),
+    }
 }
